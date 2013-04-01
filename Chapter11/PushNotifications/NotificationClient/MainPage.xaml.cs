bf5ef8b610c03c0b93d2910fdf3dda6aa2da69bc
Tim Binkley-Jones <timothybinkley@gmail.com>
2013-03-31 20:39:43 -0600

    Update notification sample to match new chapter

--- Chapter11/PushNotifications/NotificationClient/MainPage.xaml.cs
+++ Chapter11/PushNotifications/NotificationClient/MainPage.xaml.cs
@@ -8,107 +8,92 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Phone.Notification;
+using Windows.Phone.Management.Deployment;
+using Windows.ApplicationModel;
 
 namespace NotificationClient
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        private const string CHANNEL_NAME = "PushNotificationChannel";
-        HttpNotificationChannel channel;
+   public partial class MainPage : PhoneApplicationPage
+   {
+      private const string CHANNEL_NAME = "PushNotificationChannel";
+      HttpNotificationChannel channel;
 
-        public MainPage()
-        {
-            InitializeComponent();
-            SetupChannel();
-        }
+      public MainPage()
+      {
+         InitializeComponent();
+         SetupChannel();
+      }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            notificationMessage.Text = string.Format("Launched with Uri:\n{0}", e.Uri);
-        }
+      protected override void OnNavigatedTo(NavigationEventArgs e)
+      {
+         notificationMessage.Text = string.Format("Launched with Uri:\n{0}", e.Uri);
+      }
 
-        private void share_Click(object sender, EventArgs e)
-        {
-            Windows.System.Launcher.LaunchUriAsync(new System.Uri("wp8inaction:Launch?ClientChannelUri=" + channelUri.Text));
-        }
+      private void share_Click(object sender, EventArgs e)
+      {
+         IEnumerable<Package> packages = InstallationManager.FindPackagesForCurrentPublisher();
+         Package simulator = packages.FirstOrDefault(item => item.Id.Name == "NotificationSimulator");
+         if (simulator != null)
+         {
+            simulator.Launch("/MainPage.xaml?ClientChannelUri=" + channelUri.Text);
+         }
+      }
 
-        void SetupChannel()
-        {
-            bool newChannel = false;
-            channel = HttpNotificationChannel.Find(CHANNEL_NAME);
-            if (channel == null)
-            {
-                channel = new HttpNotificationChannel(CHANNEL_NAME);
-                newChannel = true;
-            }
+      void SetupChannel()
+      {
+         bool newChannel = false;
+         channel = HttpNotificationChannel.Find(CHANNEL_NAME);
+         if (channel == null)
+         {
+            channel = new HttpNotificationChannel(CHANNEL_NAME);
+            newChannel = true;
+         }
 
-            channel.ConnectionStatusChanged += channel_ConnectionStatusChanged;
-            channel.ChannelUriUpdated += channel_ChannelUriUpdated;
-            channel.ErrorOccurred += channel_ErrorOccurred;
-            channel.ShellToastNotificationReceived += channel_ShellToastNotificationReceived;
+         channel.ConnectionStatusChanged += channel_ConnectionStatusChanged;
+         channel.ChannelUriUpdated += channel_ChannelUriUpdated;
+         channel.ErrorOccurred += channel_ErrorOccurred;
+         channel.ShellToastNotificationReceived += channel_ShellToastNotificationReceived;
 
-            if (newChannel)
-            {
-                channel.Open();
-                channel.BindToShellTile();
-                channel.BindToShellToast();
-            }
+         if (newChannel)
+         {
+            channel.Open();
+            channel.BindToShellTile();
+            channel.BindToShellToast();
+         }
 
-            channelStatus.Text = channel.ConnectionStatus.ToString();
+         channelStatus.Text = channel.ConnectionStatus.ToString();
 
-            if (channel.ChannelUri != null)
-                channelUri.Text = channel.ChannelUri.ToString();
-        }
+         if (channel.ChannelUri != null)
+            channelUri.Text = channel.ChannelUri.ToString();
+      }
 
-        void channel_ConnectionStatusChanged(object sender, NotificationChannelConnectionEventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => channelStatus.Text = e.ConnectionStatus.ToString());
-        }
+      void channel_ConnectionStatusChanged(object sender, NotificationChannelConnectionEventArgs e)
+      {
+         Dispatcher.BeginInvoke(() => channelStatus.Text = e.ConnectionStatus.ToString());
+      }
 
-        void channel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => channelUri.Text = e.ChannelUri.ToString());
-        }
+      void channel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+      {
+         Dispatcher.BeginInvoke(() => channelUri.Text = e.ChannelUri.ToString());
+      }
 
-        void channel_ErrorOccurred(object sender, NotificationChannelErrorEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine(e.GetType().Name + ": " + e.Message);
-            Dispatcher.BeginInvoke(() => MessageBox.Show(e.Message, e.GetType().Name, MessageBoxButton.OK));
-        }
+      void channel_ErrorOccurred(object sender, NotificationChannelErrorEventArgs e)
+      {
+         //System.Diagnostics.Debug.WriteLine(e.GetType().Name + ": " + e.Message);
+         Dispatcher.BeginInvoke(() => MessageBox.Show(e.Message, e.GetType().Name, MessageBoxButton.OK));
+      }
 
-        void channel_ShellToastNotificationReceived(object sender, NotificationEventArgs e)
-        {
-            string title, content, parameter;
+      void channel_ShellToastNotificationReceived(object sender, NotificationEventArgs e)
+      {
+         string title, content, parameter;
+         e.Collection.TryGetValue("wp:Text1", out title);
+         e.Collection.TryGetValue("wp:Text2", out content);
+         e.Collection.TryGetValue("wp:Param", out parameter);
 
-            e.Collection.TryGetValue("wp:Text1", out title);
-            e.Collection.TryGetValue("wp:Text2", out content);
-            e.Collection.TryGetValue("wp:Param", out parameter);
+         string message = string.Format("Toast notification received.\nTitle: {0}\nContent: {1}\nParameter: {2}\n\n{3}",
+             title, content, parameter, DateTime.Now);
 
-            string message = string.Format("Toast notification received.\nTitle: {0}\nContent: {1}\nParameter: {2}\n\n{3}",
-                title, content, parameter, DateTime.Now);
-
-            Dispatcher.BeginInvoke(() => notificationMessage.Text = message);
-        }
-
-        private void updateTile_Click(object sender, RoutedEventArgs e)
-        {
-            string imagePath = tileDefaultImage.IsChecked.Value ? "Assets/Tiles/FlipCycleTileMedium.png" : tileBlueImage.IsChecked.Value ? "Assets/Tiles/Blue.jpg" : "Assets/Tiles/Green.jpg";
-            string backImagePath = tileBackNoImage.IsChecked.Value ? "" : tileBackBlueImage.IsChecked.Value ? "Assets/Tiles/Blue.jpg" : "Assets/Tiles/Green.jpg";
-            int badgeCount;
-            Int32.TryParse(tileBadgeCount.Text, out badgeCount);
-
-            ShellTile tile = ShellTile.ActiveTiles.First();
-            StandardTileData tileData = new StandardTileData
-            {
-                BackgroundImage = new Uri(imagePath, UriKind.Relative),
-                Count = badgeCount,
-                Title = tileTitle.Text,
-
-                BackBackgroundImage = new Uri(backImagePath, UriKind.Relative),
-                BackTitle = tileBackTitle.Text,
-                BackContent = tileBackContent.Text,
-            };
-            tile.Update(tileData);
-        }
-    }
+         Dispatcher.BeginInvoke(() => notificationMessage.Text = message);
+      }
+   }
 }
--- Chapter11/PushNotifications/NotificationSimulator/MainPage.xaml.cs
+++ Chapter11/PushNotifications/NotificationSimulator/MainPage.xaml.cs
@@ -10,96 +10,125 @@ using Microsoft.Phone.Shell;
 using Microsoft.Phone.Net.NetworkInformation;
 using System.IO.IsolatedStorage;
 using System.Threading;
+using System.Text;
 
 
 namespace NotificationSimulator
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        NotificationService service = new NotificationService();
+   public partial class MainPage : PhoneApplicationPage
+   {
+      NotificationService service = new NotificationService();
 
-        public MainPage()
-        {
-            InitializeComponent();
+      public MainPage()
+      {
+         InitializeComponent();
 
-            DeviceNetworkInformation.NetworkAvailabilityChanged +=
-                (sender, e) => LoadInformation(e.NotificationType.ToString());
+         DeviceNetworkInformation.NetworkAvailabilityChanged +=
+             (sender, e) =>
+             {
+                System.Diagnostics.Debug.WriteLine("Network changed");
+                LoadInformation(e.NotificationType.ToString());
+             };
 
-            string savedUri;
-            if (IsolatedStorageSettings.ApplicationSettings.TryGetValue("ChannelUri", out savedUri))
-            {
-                channelUri.Text = savedUri;
-            }
-        }
+         string savedUri;
+         if (IsolatedStorageSettings.ApplicationSettings.TryGetValue("ChannelUri", out savedUri))
+         {
+            channelUri.Text = savedUri;
+         }
+      }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            if (NavigationContext.QueryString.ContainsKey("ClientChannelUri"))
-            {
-                channelUri.Text = NavigationContext.QueryString["ClientChannelUri"];
-            }
-            LoadInformation("OnNagivatedTo");
-        }
+      protected override void OnNavigatedTo(NavigationEventArgs e)
+      {
+         if (NavigationContext.QueryString.ContainsKey("ClientChannelUri"))
+         {
+            channelUri.Text = NavigationContext.QueryString["ClientChannelUri"];
+            pivot.SelectedIndex = 1;
+            
+         }
+         LoadInformation("OnNagivatedTo");
+      }
 
-        private void LoadInformation(string trigger)
-        {
-            //System.Diagnostics.Debug.WriteLine(trigger);
-            statusMessage.Text = "loading...";
-            ThreadPool.QueueUserWorkItem((state) =>
-            {
-                string information = string.Format("Information triggered by: {0}\n\n{1}\n{2}",
-                    trigger,
-                    LoadPhoneNetworkInformation(),
-                    LoadDeviceInformation());
+      private void LoadInformation(string trigger)
+      {
+         //System.Diagnostics.Debug.WriteLine(trigger);
+         statusMessage.Text = "loading...";
+         ThreadPool.QueueUserWorkItem((state) =>
+         {
+            string information = string.Format("Information triggered by: {0}\n\n{1}\n{2}\n{3}",
+                trigger,
+                LoadPhoneNetworkInformation(),
+                LoadNetworkInterfaceInfo(),
+                LoadDeviceInformation());
 
-                Dispatcher.BeginInvoke(() => statusMessage.Text = information);
-            });
-        }
+            Dispatcher.BeginInvoke(() => statusMessage.Text = information);
+         });
+      }
 
-        private string LoadPhoneNetworkInformation()
-        {
-            return string.Format("Phone network type: {0}\nPhone network available: {1}\n",
-                NetworkInterface.NetworkInterfaceType,
-                NetworkInterface.GetIsNetworkAvailable());
-        }
+      private string LoadPhoneNetworkInformation()
+      {
+         return String.Format("Phone network type: {0}\nPhone network available: {1}\n",
+               NetworkInterface.NetworkInterfaceType,
+               NetworkInterface.GetIsNetworkAvailable());
+      }
 
-        private string LoadDeviceInformation()
-        {
-            return string.Format("Cellular operator: {0}\nCellular data enabled: {1}\n" +
-                "Roaming enabled: {2}\nDevice network available: {3}\nWi-Fi enabled: {4}\n",
-                DeviceNetworkInformation.CellularMobileOperator,
-                DeviceNetworkInformation.IsCellularDataEnabled,
-                DeviceNetworkInformation.IsCellularDataRoamingEnabled,
-                DeviceNetworkInformation.IsNetworkAvailable,
-                DeviceNetworkInformation.IsWiFiEnabled);
-        }
+      private string LoadNetworkInterfaceInfo()
+      {
+         StringBuilder builder = new StringBuilder();
+         NetworkInterfaceList list = new NetworkInterfaceList();
+         foreach (NetworkInterfaceInfo networkInterface in list)
+         {
+            builder.AppendFormat("  {0}-{1} ({2}Mbps)\n",
+               networkInterface.InterfaceType, networkInterface.InterfaceSubtype, networkInterface.Bandwidth / 1000);
+            //System.Diagnostics.Debug.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}",
+            //   networkInterface.Bandwidth, networkInterface.Characteristics, networkInterface.Description,
+            //   networkInterface.InterfaceName, networkInterface.InterfaceState, networkInterface.InterfaceSubtype,
+            //   networkInterface.InterfaceType);
+         }
+         return builder.ToString();
+      }
 
-        private void sendToast_Click(object sender, RoutedEventArgs e)
-        {
-            Uri channel;
-            if (Uri.TryCreate(channelUri.Text, UriKind.Absolute, out channel))
-            {
-                service.SendToast(channel, toastTitle.Text, toastContent.Text,
-                    string.Format("/MainPage.xaml?value1={0}&amp;value2={1}", toastValue1.Text, toastvalue2.Text));
-            }
-        }
+      private string LoadDeviceInformation()
+      {
+         return string.Format("Cellular operator: {0}\nCellular data enabled: {1}\n" +
+             "Roaming enabled: {2}\nDevice network available: {3}\nWi-Fi enabled: {4}\n",
+             DeviceNetworkInformation.CellularMobileOperator,
+             DeviceNetworkInformation.IsCellularDataEnabled,
+             DeviceNetworkInformation.IsCellularDataRoamingEnabled,
+             DeviceNetworkInformation.IsNetworkAvailable,
+             DeviceNetworkInformation.IsWiFiEnabled);
+      }
 
-        private void sendTile_Click(object sender, RoutedEventArgs e)
-        {
-            Uri channel;
-            if (Uri.TryCreate(channelUri.Text, UriKind.Absolute, out channel))
-            {
-                string imagePath = tileDefaultImage.IsChecked.Value ? "Assets/Tiles/FlipCycleTileMedium.png" : tileBlueImage.IsChecked.Value ? "Assets/Tiles/Blue.jpg" : "Assets/Tiles/Green.jpg";
-                string backImagePath = tileBackNoImage.IsChecked.Value ? "" : tileBackBlueImage.IsChecked.Value ? "Assets/Tiles/Blue.jpg" : "Assets/Tiles/Green.jpg";
-                int badgeCount;
-                Int32.TryParse(tileBadgeCount.Text, out badgeCount);
-                service.SendTile(channel, imagePath, badgeCount, tileTitle.Text, backImagePath, tileBackTitle.Text, tileBackContent.Text);
-            }
-        }
+      private void sendToast_Click(object sender, RoutedEventArgs e)
+      {
+         Uri channel;
+         if (Uri.TryCreate(channelUri.Text, UriKind.Absolute, out channel))
+         {
+            service.SendToast(channel, toastTitle.Text, toastContent.Text,
+                string.Format("/MainPage.xaml?value1={0}&amp;value2={1}", toastValue1.Text, toastvalue2.Text));
+         }
+      }
 
-        private void stackPanel_Hold(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            IsolatedStorageSettings.ApplicationSettings["ChannelUri"] = channelUri.Text;
-        }
-    }
+      private void sendTile_Click(object sender, RoutedEventArgs e)
+      {
+         Uri channel;
+         if (Uri.TryCreate(channelUri.Text, UriKind.Absolute, out channel))
+         {
+            string imagePath = tileDefaultImage.IsChecked.Value ? "/Assets/Tiles/FlipCycleTileMedium.png" : tileBlueImage.IsChecked.Value ? "/Assets/Tiles/Blue.png" : "/Assets/Tiles/Green.png";
+            string backImagePath = tileBackNoImage.IsChecked.Value ? "" : tileBackBlueImage.IsChecked.Value ? "/Assets/Tiles/Blue.png" : "/Assets/Tiles/Green.png";
+            int badgeCount;
+            Int32.TryParse(tileBadgeCount.Text, out badgeCount);
+            service.SendTile(channel, imagePath, badgeCount, tileTitle.Text, backImagePath, tileBackTitle.Text, tileBackContent.Text);
+         }
+      }
+
+      private void stackPanel_Hold(object sender, System.Windows.Input.GestureEventArgs e)
+      {
+         IsolatedStorageSettings.ApplicationSettings["ChannelUri"] = channelUri.Text;
+      }
+
+      private void Button_Click(object sender, RoutedEventArgs e)
+      {
+         LoadInformation("update status");
+      }
+   }
 }
