d39a741210ca5745d2e50441ae269df718f9c35a
Tim Binkley-Jones <timothybinkley@gmail.com>
2013-04-05 12:05:55 -0600

    Update MediaPlayback project to match new book
        Chapter 17 in the new books adds coverage of the MediaPlayerLauncher and
        removes coverage of MediaStreamSource and Smooth Streaming.

--- Chapter15/MediaPlayback/MainPage.xaml.cs
+++ Chapter15/MediaPlayback/MainPage.xaml.cs
@@ -11,117 +11,129 @@ using MediaPlayback.Resources;
 using System.IO.IsolatedStorage;
 using System.IO;
 using System.Windows.Media;
+using Windows.Storage;
+using Microsoft.Phone.Tasks;
+
 
 namespace MediaPlayback
 {
-    public partial class MainPage : PhoneApplicationPage
-    {
-        // Constructor
-        public MainPage()
-        {
-            InitializeComponent();
+   public partial class MainPage : PhoneApplicationPage
+   {
+      // Constructor
+      public MainPage()
+      {
+         InitializeComponent();
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
-        }
+         // Sample code to localize the ApplicationBar
+         //BuildLocalizedApplicationBar();
+      }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+      // Sample code for building a localized ApplicationBar
+      //private void BuildLocalizedApplicationBar()
+      //{
+      //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+      //    ApplicationBar = new ApplicationBar();
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+      //    // Create a new button and set the text value to the localized string from AppResources.
+      //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+      //    appBarButton.Text = AppResources.AppBarButtonText;
+      //    ApplicationBar.Buttons.Add(appBarButton);
 
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+      //    // Create a new menu item with the localized string from AppResources.
+      //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+      //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+      //}
 
-        private void PlayClicked(object sender, EventArgs e)
-        {
-            mediaElement.Play();
-        }
+      private void LaunchVideoFromInstall_Click(object sender, EventArgs e)
+      {
+         var task = new MediaPlayerLauncher();
+         task.Location = MediaLocationType.Install;
+         task.Media = new Uri("Assets/sample.wmv", UriKind.Relative);
+         task.Show();
+      }
 
-        private void PauseClicked(object sender, EventArgs e)
-        {
-            mediaElement.Pause();
-        }
+      private void LaunchVideoFromStorage_Click(object sender, EventArgs e)
+      {
+         var task = new MediaPlayerLauncher();
+         task.Media = new Uri("sample.wmv", UriKind.Relative);
+         task.Show();
+      }
 
-        private void StopClicked(object sender, EventArgs e)
-        {
-            mediaElement.Stop();
-        }
+      private void LaunchVideoFromWeb_Click(object sender, EventArgs e)
+      {
+         var task = new MediaPlayerLauncher();
+         task.Location = MediaLocationType.None;
+         task.Media = new Uri("http://www.windowsphoneinaction.com/sample.wmv");
+         task.Show();
+      }
 
-        private void MuteClicked(object sender, EventArgs e)
-        {
-            mediaElement.IsMuted = !mediaElement.IsMuted;
-            mutedTextBlock.Text = mediaElement.IsMuted ? "muted" : string.Empty;
-        }
+      private void VideoFromInstall_Click(object sender, EventArgs e)
+      {
+         mediaElement.Source = new Uri("Assets/sample2.wmv", UriKind.Relative);
+         sourceTextBlock.Text = "video from install";
+      }
 
-        private void VideoFromXapClicked(object sender, EventArgs e)
-        {
-            mediaElement.Source = new Uri("Media/sample.wmv", UriKind.Relative);
-            sourceTextBlock.Text = "video from xap";
-        }
-
-        private void VideoFromStorageClicked(object sender, EventArgs e)
-        {
-            mediaElement.Source = null;
-            using (var store = IsolatedStorageFile.GetUserStoreForApplication())
+      private void VideoFromStorage_Click(object sender, EventArgs e)
+      {
+         mediaElement.Source = null;
+         using (var store = IsolatedStorageFile.GetUserStoreForApplication())
+         {
+            if (store.FileExists("sample.wmv"))
             {
-                if (store.FileExists("sample.wmv"))
-                {
-                    var fileStream = new IsolatedStorageFileStream("sample.wmv", FileMode.Open, store);
-                    mediaElement.SetSource(fileStream);
-                    sourceTextBlock.Text = "video from storage";
-                }
+               var fileStream = new IsolatedStorageFileStream("sample.wmv", FileMode.Open, store);
+               mediaElement.SetSource(fileStream);
+               sourceTextBlock.Text = "video from storage";
             }
-        }
+            sourceTextBlock.Text = "video from storage";
+         }
+      }
 
-        private void VideoFromWebClicked(object sender, EventArgs e)
-        {
-            mediaElement.Source = new Uri(
-                "http://www.wp7inaction.com/sample.wmv",
-                UriKind.Absolute);
-            sourceTextBlock.Text = "video from web";
-        }
+      private void VideoFromWeb_Click(object sender, EventArgs e)
+      {
+         mediaElement.Source = new Uri("http://www.windowsphoneinaction.com/sample.wmv");
+         sourceTextBlock.Text = "video from web";
+      }
 
-        private void CustomVideoClicked(object sender, EventArgs e)
-        {
-            mediaElement.Source = null;
-            MediaStreamSource source = new VideoMediaStreamSource(
-                (int)mediaElement.ActualWidth,
-                (int)mediaElement.ActualHeight);
-            mediaElement.SetSource(source);
-            sourceTextBlock.Text = "custom video";
-        }
+      private void Play_Click(object sender, EventArgs e)
+      {
+         mediaElement.Play();
+      }
 
-        private void CustomAudioClicked(object sender, EventArgs e)
-        {
-            mediaElement.Source = null;
-            MediaStreamSource source = new AudioMediaStreamSource(440);
-            mediaElement.SetSource(source);
-            sourceTextBlock.Text = "custom audio";
-        }
+      private void Pause_Click(object sender, EventArgs e)
+      {
+         mediaElement.Pause();
+      }
 
-        private void mediaElement_CurrentStateChanged(object sender, RoutedEventArgs e)
-        {
-            stateTextBlock.Text = mediaElement.CurrentState.ToString();
-            if (mediaElement.CurrentState == MediaElementState.Opening)
-                //mediaProgress.Visibility = Visibility.Visible;
-                mediaProgress.IsVisible = true;
-            else
-                //mediaProgress.Visibility = Visibility.Collapsed;
-                mediaProgress.IsVisible = false;
-        }
+      private void Stop_Click(object sender, EventArgs e)
+      {
+         mediaElement.Stop();
+      }
 
-        private void mediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
-        {
-            MessageBox.Show(e.ErrorException.Message, "Media Failure", MessageBoxButton.OK);
-        }
-    }
+      private void Mute_Click(object sender, EventArgs e)
+      {
+         mediaElement.IsMuted = !mediaElement.IsMuted;
+         mutedTextBlock.Text = mediaElement.IsMuted ? "muted" : string.Empty;
+      }
+
+      private void mediaElement_CurrentStateChanged(object sender, RoutedEventArgs e)
+      {
+         stateTextBlock.Text = mediaElement.CurrentState.ToString();
+         if (mediaElement.CurrentState == MediaElementState.Opening)
+            mediaProgress.IsVisible = true;
+         else
+            mediaProgress.IsVisible = false;
+      }
+
+      private void mediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+      {
+         MessageBox.Show(e.ErrorException.Message, "Media Failure", MessageBoxButton.OK);
+      }
+
+      private void ApplicationBarMenuItem_Click_1(object sender, EventArgs e)
+      {
+
+      }
+
+
+   }
 }
